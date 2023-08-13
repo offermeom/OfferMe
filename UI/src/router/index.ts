0650986e1da4a714772dab670ee433186749743d
naren <naren000000000@gmail.com>
2023-08-13 13:14:36 +0530

    added code changes

--- UI/src/router/index.ts
+++ UI/src/router/index.ts
@@ -1,21 +1,65 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   route level code-splitting
+    //   this generates a separate chunk (About.[hash].js) for this route
+    //   which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/ChoiceScreen.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/loginvendor',
+      name: 'loginvendor',
+      component: () => import('../views/vendor/LoginVendor.vue')
+    },
+    {
+      path: '/registervendor',
+      name: 'registervendor',
+      component: () => import('../views/vendor/RegisterVendor.vue')
+    },
+    {
+      path: '/vendorhomescreen',
+      name: 'vendorhomescreen',
+      component: () => import('../views/vendor/VendorHomeScreen.vue')
+    },
+    {
+      path: '/vendoraddoffer',
+      name: 'vendoraddoffer',
+      component: () => import('../views/vendor/VendorAddOffer.vue')
+    },
+    {
+      path: '/vendorviewoffer',
+      name: 'vendorviewoffer',
+      component: () => import('../views/vendor/VendorViewOffer.vue')
+    },
+    {
+      path: '/logincustomer',
+      name: 'logincustomer',
+      component: () => import('../views/customer/LoginCustomer.vue')
+    },
+    {
+      path: '/registercustomer',
+      name: 'registercustomer',
+      component: () => import('../views/customer/RegisterCustomer.vue')
+    },
+    {
+      path: '/customerhomescreen',
+      name: 'customerhomescreen',
+      component: () => import('../views/customer/CustomerHomeScreen.vue')
     }
   ]
 })
